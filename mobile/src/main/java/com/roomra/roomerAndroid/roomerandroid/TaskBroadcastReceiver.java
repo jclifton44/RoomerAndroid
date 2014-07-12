de92dc9064ec5f3a6ab5ee5beaf9157d3806ebd0
Jeremy Clifton <jclifton44@gmail.com>
2014-07-12 17:21:02 -0500

    TaskBroadcastReceiver

--- mobile/src/main/java/com/roomra/roomerAndroid/roomerandroid/TaskBroadcastReceiver.java
+++ mobile/src/main/java/com/roomra/roomerAndroid/roomerandroid/TaskBroadcastReceiver.java
@@ -3,13 +3,16 @@ import android.content.BroadcastReceiver;
 import android.os.PowerManager;
 import android.content.Context;
 import android.content.Intent;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+
 
 
 
 /**
  * Created by jeremyclifton on 7/12/14.
  */
-public class TimedBroadcastReceiver extends BroadcastReceiver {
+public class TaskBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         PowerManager.WakeLock battery = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, RoomerConstants.LOCK_TAG);
@@ -19,7 +22,10 @@ public class TimedBroadcastReceiver extends BroadcastReceiver {
 
         battery.release();    //Releasing Semaphore for sleep/wake
     }
-    public void setAlarm(Context context){
+    public void setAlarm(Context context, String taskObject){
+        AlarmManager alarm = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, TaskBroadcastReceiver.class);
+        PendingIntent pendingTask = PendingIntent.getBroadcast(context, 0, intent, 0);
 
     }
     public void cancelAlarm(Context context){
