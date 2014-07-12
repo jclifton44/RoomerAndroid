a3f37770b3ffa204bba37a80b834b50cac814481
Jeremy Clifton <jclifton44@gmail.com>
2014-07-12 17:05:43 -0500

    Adding to broadcast receiver

--- mobile/src/main/java/com/roomra/roomerAndroid/roomerandroid/RoomerConstants.java
+++ mobile/src/main/java/com/roomra/roomerAndroid/roomerandroid/RoomerConstants.java
@@ -6,4 +6,6 @@ package com.roomra.roomerAndroid.roomerandroid;
 public class RoomerConstants {
     public static final String ROOMRA_URL="https://roomra.com/";
     public static final String PREFS_FILE = "roomra";
+    public static final String LOCK_TAG = "roomra";
+
 }
--- mobile/src/main/java/com/roomra/roomerAndroid/roomerandroid/TimedBroadcastReceiver.java
+++ mobile/src/main/java/com/roomra/roomerAndroid/roomerandroid/TimedBroadcastReceiver.java
@@ -1,12 +1,23 @@
 package com.roomra.roomerAndroid.roomerandroid;
 import android.content.BroadcastReceiver;
+import android.os.PowerManager;
+import android.content.Context;
+import android.content.Intent;
+
+
 
 /**
  * Created by jeremyclifton on 7/12/14.
  */
 public class TimedBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock battery = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, RoomerConstants.LOCK_TAG);
+        battery.acquire();    //Acquiring Semaphore for sleep/wake
 
+        //do shit
+
+        battery.release();    //Releasing Semaphore for sleep/wake
     }
     public void setAlarm(Context context){
 
