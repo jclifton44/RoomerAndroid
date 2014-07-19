88b8b77a2b3d1fa1db01f176bde5a6c87ebf78aa
Jeremy Clifton <jclifton44@gmail.com>
2014-07-19 03:18:52 -0500

    Adding swipe to refresh

--- mobile/src/main/java/com/roomra/roomerAndroid/roomerandroid/FrontPagePosts.java
+++ mobile/src/main/java/com/roomra/roomerAndroid/roomerandroid/FrontPagePosts.java
@@ -2,19 +2,33 @@ package com.roomra.roomerAndroid.roomerandroid;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FrontPagePosts extends Fragment {
-
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
 
         View rootView = inflater.inflate(R.layout.front_page_posts, container, false);
-
+        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_container);
         return rootView;
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i("SWIPE", "onRefresh called from SwipeRefreshLayout");
+
+                //initiateRefresh();
+            }
+        });
     }
 }
